0ea9dbaf3097cdf94878e99ee774b2038a198329
zainikh <139334991+zaiinkhalid@users.noreply.github.com>
2023-07-13 13:48:07 +0500

    Add files via upload

---
+++ titanic_sur.py
@@ -0,0 +1,36 @@
+#data analysis libraries
+import pandas as pd
+
+#visualization libraries
+import seaborn as sns
+%matplotlib inline
+train = pd.read_csv("../input/train.csv")
+test = pd.read_csv("../input/test.csv")
+
+#take a look at the training data
+train.describe(include="all")
+#get a list of the features within the dataset
+print(train.columns)
+Index(['PassengerId', 'Survived', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp',
+       'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'],
+      dtype='object')
+train.describe(include = "all")
+print(pd.isnull(train).sum())
+#draw a bar plot of survival by sex
+sns.barplot(x="Sex", y="Survived", data=train)
+
+#print percentages of females vs. males that survive
+print("Percentage of females who survived:", train["Survived"][train["Sex"] == 'female'].value_counts(normalize = True)[1]*100)
+
+print("Percentage of males who survived:", train["Survived"][train["Sex"] == 'male'].value_counts(normalize = True)[1]*100)
+
+
+#draw a bar plot of survival by Pclass
+sns.barplot(x="Pclass", y="Survived", data=train)
+
+#print percentage of people by Pclass that survived
+print("Percentage of Pclass = 1 who survived:", train["Survived"][train["Pclass"] == 1].value_counts(normalize = True)[1]*100)
+
+print("Percentage of Pclass = 2 who survived:", train["Survived"][train["Pclass"] == 2].value_counts(normalize = True)[1]*100)
+
+print("Percentage of Pclass = 3 who survived:", train["Survived"][train["Pclass"] == 3].value_counts(normalize = True)[1]*100)
